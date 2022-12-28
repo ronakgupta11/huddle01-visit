5cdcfb4144ce8c587cf21ba6b3a950b15aec5757
Ronak Gupta <mailtoronakgupta@gmail.com>
2022-12-28 21:34:12 +0530

    change file structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,105 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/home';
+// import Home from './components/home';
 import Main from './components/main';
 import {HuddleClientProvider, getHuddleClient } from '@huddle01/huddle01-client';
 import MeVideoElem from './components/MeVideoElem';
 import PeerVideoAudioElem from './components/PeerVideoAudioElem';
 import { useState } from 'react';
 import { useHuddleStore } from "@huddle01/huddle01-client/store";
-// import Notification from './components/Notification';
+import {ethers} from "ethers";
+import Web3Modal from "web3modal";
+import * as PushAPI from "@pushprotocol/restapi";
+
+import Notification from './components/Notification';
 
 
 function App() {
+
   const peersKeys = useHuddleStore((state) => Object.keys(state.peers));
   const lobbyPeers = useHuddleStore((state) => state.lobbyPeers);
   const [address, setAddress] = useState("");
   const huddleClient = getHuddleClient("702b03a76c58010686023dac1caeb63696b04b1c069ef14405b4ede34ed1586b");
   
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3Modal = new Web3Modal({
+        network: "goerli",
+        providerOptions : {},
+        disableInjectedProvider: false,
+      });
+
+    let signer;
+    let provider; 
+
+    let instance;
+
+    async function connectWallet(){
+
+        instance = await web3Modal.connect();
+        provider = new ethers.providers.Web3Provider(instance);
+        signer = provider.getSigner();
+        setAddress(await signer.getAddress());
+        setWalletConnected(true);
+        console.log(signer);
+        }
+    console.log(signer);
+    async function disconnect(){
+        await web3Modal.clearCachedProvider();
+        provider = null;
+        signer = null;
+        setWalletConnected(false);
+        renderButton();
+        setAddress("");
+    }
+    function renderButton(){
+        if(walletConnected){
+            return(
+                <div>
+
+                <p>wallet address = {address}</p>
+                <button onClick={disconnect}>disconnect wallet</button>
+                </div>
+            )
+        }
+        else{
+            return(
+                <button onClick={connectWallet}>Connect Wallet</button>
+            )
+        }
+    }
   
   return (
     <HuddleClientProvider client = {huddleClient} >
 
     <div className="App">
-      <Home address = {address} set = {setAddress}/>
+    <div className="hom">
+            <h1 className='heading'>Caller</h1>
+            <p className='des'>decentralized wallet to wallet video calling platform.</p>
+            {renderButton()}
+            {/* <button onClick={sendNotification}>send</button>      */}
+        </div>
+      {/* <Home address = {address} set = {setAddress}/> */}
       <Main address = {address} client = {huddleClient}/>
+      <Notification connect = {connectWallet} signer = {signer}/>
       <MeVideoElem/>
+      <div className='peer-section'>
+
       <p>---------peer section------------</p>
-      {/* {lobbyPeers[0] && <h2>Lobby Peers</h2>}
+      {lobbyPeers[0] && <h2>Lobby Peers</h2>}
           <div>
-            {lobbyPeers.map((peer) => (
-              <div>{peer.peerId}</div>
+          {lobbyPeers.map((peer) => (
+            <div>{peer.peerId}</div>
             ))}
-          </div>
-
-          {peersKeys[0] && <h2>Peers</h2>}
-
-          <div className="peers-grid">
+            </div>
+            
+            {peersKeys[0] && <h2>Peers</h2>}
+            
+            <div className="peers-grid">
             {peersKeys.map((key) => (
               <PeerVideoAudioElem key={`peerId-${key}`} peerIdAtIndex={key} />
-            ))}
-          </div> */}
-        {/* <Notification/> */}
+              ))}
+            </div>
+            </div>
     </div>
     </HuddleClientProvider>
     
--- my-app/src/components/Notification.js
+++ my-app/src/components/Notification.js
@@ -1,45 +1,106 @@
 import * as PushAPI from "@pushprotocol/restapi";
 import { NotificationItem, chainNameType } from "@pushprotocol/uiweb";
+import { useState } from "react";
 
-export default function Notification(){
-    async function getNot(){
-    const notifications = await PushAPI.user.getFeeds({
-        user: 'eip155:5:0x294d985B6BC5dA375b571B5fDE228334343f4EdF', // user address in CAIP
-        env: 'staging'
-      });
+export default function Notification(props){
+    const signer = props.signer;
+    let notifications;
+      const [rendered,setRendered] = useState([]);
+      async function getNot(){
+      notifications = await PushAPI.user.getFeeds({
+            user: 'eip155:5:0x294d985B6BC5dA375b571B5fDE228334343f4EdF', // user address in CAIP
+            env: 'staging'
+          });
+        console.log(notifications)
+      
+      // getNot();
+      const rendered_not = notifications.map((oneNotification, i) => {
+        const { 
+            cta,
+            title,
+            message,
+            app,
+            icon,
+            image,
+            url,
+            blockchain,
+            notification
+        } = oneNotification;
+    
+        return (
+            <NotificationItem
+                key={`notif-${i}`} // any unique id
+                notificationTitle={title}
+                notificationBody={message}
+                cta={cta}
+                app={app}
+                icon={icon}
+                image={image}
+                url={url}
+                theme="light"
+                chainName={blockchain}
+                // chainName={blockchain as chainNameType} // if using Typescript
+            />
+            );
+          });
+          setRendered(rendered_not);
+          // console.log("rendered-notif",rendered);
+          // return(
+          //   <div>{rendered}</div>
+          // );
+        }
+        const sendNotification = async() => {
+            try {
+    
+              props.connect()
+              console.log("signer in send",signer)
+            //   let subscriptions = await PushAPI.user.getSubscriptions({
+            //     user: 'eip155:5:0xD7D98e76FcD14689F05e7fc19BAC465eC0fF4161', // user address in CAIP
+            //     env: 'staging'
+            //   });
+            //   console.log(subscriptions)
+    
+    
+    
+              const apiResponse = await PushAPI.payloads.sendNotification({
+                signer,
+                type: 3, // target
+                identityType: 2, // direct payload
+                notification: {
+                  title: `[SDK-TEST] notification TITLE: test`,
+                  body: `[sdk-test] notification BODY Testing notification`
+                },
+                payload: {
+                  title: `call requested from ${await signer.getAddress()}`,
+                  body: `to join call go to bellow link`,
+                  cta: 'www.about.com',
+                  img: ''
+                },
+                recipients: 'eip155:5:0x294d985B6BC5dA375b571B5fDE228334343f4EdF', // recipient address
+                channel: 'eip155:5:0xD7D98e76FcD14689F05e7fc19BAC465eC0fF4161', // your channel address
+                env: 'staging'
+              });
+              
+              // apiResponse?.status === 204, if sent successfully!
+              console.log('API repsonse: ', apiResponse);
+            } catch (err) {
+              console.error('Error: ', err);
+            }
+          }
+        return(
+        <div className="notification-section">
 
-{notifications.map((oneNotification, i) => {
-    const { 
-        cta,
-        title,
-        message,
-        app,
-        icon,
-        image,
-        url,
-        blockchain,
-        notification
-    } = oneNotification;
+                    
+        <div className="get-notif">
+        {console.log("render before-",rendered)}
+  
+        {rendered}
+          </div> 
+        <div className="send-notif">
+        <button onClick={sendNotification}>send</button>     
 
-    return (
-        <NotificationItem
-            key= {i} // any unique id
-            notificationTitle={title}
-            notificationBody={message}
-            cta={cta}
-            app={app}
-            icon={icon}
-            image={image}
-            url={url}
-            theme="dark"
-            chainName={blockchain}
-            // chainName={blockchain as chainNameType} // if using Typescript
-        />
-        );
-    })}
-}
-return(
-    <button onClick={getNot}>get Notification</button>
-)
 
+        </div>
+        </div>
+        )
 }
